724cfbc5a2221a5a9504750e87dc3fbc34e263f5
Galina Psch <over.passover@gmail.com>
2020-05-14 06:12:20 +0300

    eslinted

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
