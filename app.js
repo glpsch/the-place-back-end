b6c4369440e44680282f0af5fa8ff34c0812ce49
Galina Psch <over.passover@gmail.com>
2020-05-24 17:44:25 +0300

    models

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const routerCards = require('./routes/cards.js');
 const routerUsers = require('./routes/users.js');
@@ -8,6 +9,14 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 app.listen(PORT);
+
+//
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+//
 
 
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ models/cards.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    // url
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true, /* ? */
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
+
+// link — ссылка на картинку, строка, обязательно поле;
+// owner — ссылка на модель автора карточки, тип ObjectId, обязательное поле;
+// likes — список лайкнувших пост пользователей, массив ObjectId, по умолчанию
+// — пустой массив(поле default)
+//   createdAt — дата создания, тип Date, значение по умолчанию Date.now.
