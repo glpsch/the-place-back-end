bdc377531e13cbdfff8f0eca1e8d5ee045f83caf
Galina Psch <over.passover@gmail.com>
2020-06-20 04:38:50 +0300

    joi

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+
 const { PORT, DATABASE_URL } = require('./config');
 const routerCards = require('./routes/cards.js');
 const routerUsers = require('./routes/users.js');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+
 
 const app = express();
 app.listen(PORT);
@@ -25,9 +29,23 @@ mongoose.connect(DATABASE_URL, {
   useUnifiedTopology: true,
 });
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), login);
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().uri(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), createUser);
+
 
 app.use(cookieParser());
 app.use(auth);
@@ -35,10 +53,7 @@ app.use(auth);
 app.use('/cards', routerCards);
 app.use('/users', routerUsers);
 
-
-app.all('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
+app.use(errors()); // обработчик ошибок celebrate
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
@@ -51,4 +66,8 @@ app.use((err, req, res, next) => {
         ? 'На сервере произошла ошибка'
         : message,
     });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,16 @@
 
 const routerCards = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const { getAllCards, createCard, deleteCard } = require('../controllers/cards');
 
 routerCards.get('/', getAllCards);
-routerCards.post('/', createCard);
+routerCards.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+}), createCard);
 routerCards.delete('/:cardId', deleteCard);
 
 module.exports = routerCards;
